80e7909f93d932f6b5ecd49c3cee407489685fea
getulio.souza <getulio.souza@evolutix.com.br>
2023-02-09 15:23:13 -0300

    refatorando o codigo

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,15 +1,15 @@
 export class Negociacao {
-    private _data: Date;
-    private _quantidade: number;
-    private _valor: number;
+    // private _data: Date;
+    // private _quantidade: number;
+    // private _valor: number;
 
     //esses valores são passados para dentro de um construtor
 
-    constructor(data: Date, quantidade: number, valor: number) {
-        this._data = data;
-        this._quantidade = quantidade;
-        this._valor = valor;
-    }
+    constructor(
+        private _data: Date, 
+        private _quantidade: number, 
+        private _valor: number
+        ) {}
 
     get data(): Date {
         return this.data;
--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -4,7 +4,7 @@ import { Negociacao } from "./negociacao.js";
 
 //essa classe guarda, de maneira privada, uma lista de negociações
 export class Negociacoes {
-    private negociacoes: Array<Negociacao> = [];
+    private negociacoes: Negociacao[] = [];
     
     //recebo um parametro 'negociacao' do tipo 'negociacoes" e 
     adiciona(negociacao: Negociacao) {
@@ -13,7 +13,10 @@ export class Negociacoes {
     }
 
     //vamos retornar a lista que será rendenizada na tela 
-    lista(): Array<Negociacao>{
+    // o read only array retorna somente uma lista do tipo leitura 
+    lista(): readonly Negociacao[] {
+        //podemos usar o spread operador para pegar cada item dessa lista e colocá-lo dentro de uma nova lista
+        // return [...this.negociacoes];
         return this.negociacoes;
     }
 }
