7ae692cd95d3f45bb2d53c990cc853b0c87443e9
getulio.souza <getulio.souza@evolutix.com.br>
2023-02-13 11:29:39 -0300

    criando-tabela-declarativa

---
+++ app/views/negociacoes-view.ts
@@ -0,0 +1,24 @@
+//vamos criar uma tabela de forma declarativa 
+
+export class NegociacoesView {
+    template (): string {
+        return `
+         <table class="table table-hover table-bordered">
+            <thead>
+             <tr>
+                <th>Data/th>
+                <th>Quantidade/th>
+                <th>Valor/th>
+             </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    </tr>
+                </tbody>
+         </table>
+        `;
+    }
+}
