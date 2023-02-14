670104bb73c3d46511e68730dfd7e1506807ceb9
getulio.souza <getulio.souza@evolutix.com.br>
2023-02-14 09:38:17 -0300

    comentario

--- app/views/negociacoes-view.ts
+++ app/views/negociacoes-view.ts
@@ -1,7 +1,7 @@
 //vamos criar uma tabela de forma declarativa 
-// o template é um método que é iniciado como tipo string que será a nossa tabela renderizada 
 export class NegociacoesView {
     template (): string {
+        // o template é um método que é iniciado como tipo string que será a nossa tabela renderizada 
         return `
          <table class="table table-hover table-bordered">
             <thead>
